5d72e82bfb2f78ca9763dc44e6d0b3374023186c
tpy <p.truesword@gmail.com>
2012-09-18 06:44:04 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StillThere::Application.config.secret_token = '33f6b04338953f16def5f6a23ad3342ab02848a6f66278e8d189ef9bd166104d5f4e9e6ab7f0c12acf4de1d103202f3e3ae293e614a2167b9734a80ca07d5c15'
